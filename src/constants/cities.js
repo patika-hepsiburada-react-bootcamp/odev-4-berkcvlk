6f1f3ca1de67886e820bbaf88ce1af4b0f514987
Berk Civelek <berkcvlk@gmail.com>
2021-10-18 01:35:40 +0300

    add: cities data

---
+++ src/constants/cities.js
@@ -0,0 +1,322 @@
+export const cities = [
+  {
+    id: "325361",
+    text: "Adana",
+  },
+  {
+    id: "325329",
+    text: "Adıyaman",
+  },
+  {
+    id: "325302",
+    text: "Afyonkarahisar",
+  },
+  {
+    id: "325163",
+    text: "Ağrı",
+  },
+  {
+    id: "752014",
+    text: "Amasya",
+  },
+  {
+    id: "323786",
+    text: "Ankara",
+  },
+  {
+    id: "323776",
+    text: "Antalya",
+  },
+  {
+    id: "751816",
+    text: "Artvin",
+  },
+  {
+    id: "322819",
+    text: "Aydın",
+  },
+  {
+    id: "322164",
+    text: "Balıkesir",
+  },
+  {
+    id: "321122",
+    text: "Bilecik",
+  },
+  {
+    id: "321082",
+    text: "Bingöl",
+  },
+  {
+    id: "321022",
+    text: "Bitlis",
+  },
+  {
+    id: "750510",
+    text: "Bolu",
+  },
+  {
+    id: "320392",
+    text: "Burdur",
+  },
+  {
+    id: "750268",
+    text: "Bursa",
+  },
+  {
+    id: "749778",
+    text: "Çanakkale",
+  },
+  {
+    id: "749747",
+    text: "Çankırı",
+  },
+  {
+    id: "748877",
+    text: "Çorum",
+  },
+  {
+    id: "317106",
+    text: "Denizli",
+  },
+  {
+    id: "316541",
+    text: "Diyarbakır",
+  },
+  {
+    id: "747712",
+    text: "Edirne",
+  },
+  {
+    id: "315808",
+    text: "Elazığ",
+  },
+  {
+    id: "315372",
+    text: "Erzincan",
+  },
+  {
+    id: "315367",
+    text: "Erzurum",
+  },
+  {
+    id: "315202",
+    text: "Eskişehir",
+  },
+  {
+    id: "314830",
+    text: "Gaziantep",
+  },
+  {
+    id: "746878",
+    text: "Giresun",
+  },
+  {
+    id: "312888",
+    text: "Hakkari",
+  },
+  {
+    id: "312394",
+    text: "Hatay",
+  },
+  {
+    id: "311071",
+    text: "Isparta",
+  },
+  {
+    id: "311728",
+    text: "Mersin",
+  },
+  {
+    id: "745042",
+    text: "İstanbul",
+  },
+  {
+    id: "311044",
+    text: "İzmir",
+  },
+  {
+    id: "743952",
+    text: "Kars",
+  },
+  {
+    id: "743881",
+    text: "Kastamonu",
+  },
+  {
+    id: "308463",
+    text: "Kayseri",
+  },
+  {
+    id: "743166",
+    text: "Kırklareli",
+  },
+  {
+    id: "307513",
+    text: "Kırşehir",
+  },
+  {
+    id: "742865",
+    text: "Kocaeli",
+  },
+  {
+    id: "306569",
+    text: "Konya",
+  },
+  {
+    id: "305268",
+    text: "Kütahya",
+  },
+  {
+    id: "304919",
+    text: "Malatya",
+  },
+  {
+    id: "304825",
+    text: "Manisa",
+  },
+  {
+    id: "310858",
+    text: "Kahramanmaraş",
+  },
+  {
+    id: "304797",
+    text: "Mardin",
+  },
+  {
+    id: "304183",
+    text: "Muğla",
+  },
+  {
+    id: "304041",
+    text: "Muş",
+  },
+  {
+    id: "303830",
+    text: "Nevşehir",
+  },
+  {
+    id: "303826",
+    text: "Niğde",
+  },
+  {
+    id: "741098",
+    text: "Ordu",
+  },
+  {
+    id: "740481",
+    text: "Rize",
+  },
+  {
+    id: "740352",
+    text: "Sakarya",
+  },
+  {
+    id: "740263",
+    text: "Samsun",
+  },
+  {
+    id: "300822",
+    text: "Siirt",
+  },
+  {
+    id: "739598",
+    text: "Sinop",
+  },
+  {
+    id: "300617",
+    text: "Sivas",
+  },
+  {
+    id: "738927",
+    text: "Tekirdağ",
+  },
+  {
+    id: "738742",
+    text: "Tokat",
+  },
+  {
+    id: "738647",
+    text: "Trabzon",
+  },
+  {
+    id: "298846",
+    text: "Tunceli",
+  },
+  {
+    id: "298332",
+    text: "Şanlıurfa",
+  },
+  {
+    id: "298298",
+    text: "Uşak",
+  },
+  {
+    id: "298117",
+    text: "Van",
+  },
+  {
+    id: "296560",
+    text: "Yozgat",
+  },
+  {
+    id: "737021",
+    text: "Zonguldak",
+  },
+  {
+    id: "324496",
+    text: "Aksaray",
+  },
+  {
+    id: "750938",
+    text: "Bayburt",
+  },
+  {
+    id: "443187",
+    text: "Karaman",
+  },
+  {
+    id: "443188",
+    text: "Kırıkkale",
+  },
+  {
+    id: "321836",
+    text: "Batman",
+  },
+  {
+    id: "300640",
+    text: "Şırnak",
+  },
+  {
+    id: "862467",
+    text: "Bartın",
+  },
+  {
+    id: "751952",
+    text: "Ardahan",
+  },
+  {
+    id: "311665",
+    text: "Iğdır",
+  },
+  {
+    id: "738025",
+    text: "Yalova",
+  },
+  {
+    id: "744562",
+    text: "Karabük",
+  },
+  {
+    id: "307864",
+    text: "Kilis",
+  },
+  {
+    id: "303195",
+    text: "Osmaniye",
+  },
+  {
+    id: "747764",
+    text: "Düzce",
+  },
+];
